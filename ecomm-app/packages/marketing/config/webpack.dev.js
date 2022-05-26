bc24b45d9203c8823abb805c4d43fe324a41adec
Joseph Adjare <joseph.adjare@newlook.com>
2022-05-26 21:47:35 +0100

    Lesson #46 - Reminder on Shared Modules

---
+++ ecomm-app/packages/marketing/config/webpack.dev.js
@@ -0,0 +1,28 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common.js");
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: { "./MarketingApp": "./src/bootstrap" },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
